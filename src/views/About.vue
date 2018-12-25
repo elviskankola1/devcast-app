b6720a04b08d093b753878360c94e7d35a4ed222
bernard-ng <ngandubernard@gmail.com>
2018-12-25 13:17:19 +0200

    [Update] add assets files and structure views

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,129 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <!-- ===== ABOUT US ===== -->
+			<section id="about-us" class="section-negative">
+				<div class="container">
+
+					<!-- ===== SECTION TITLE ===== -->
+					<h2 class="title-default text-center">Behind the mic...</h2>
+
+					<div class="row mb-30">
+
+						<!-- ===== TEAM CARD ===== -->
+						<div class="col-lg-4 col-lg-offset-0 col-md-8 col-md-offset-2 col-sm-12 mb-40">
+							<div class="team-card">
+								<figure class="team-card-image"><img src="assets/img/team/team-1.jpg" alt="John Doe" title="John Doe" /></figure>
+								<div class="team-card-content">
+									<h2 class="team-card-title">John Doe</h2>
+									<p class="team-card-job">Main Caster / Web Developer</p>
+									<p class="team-card-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat itaque optio minus perferendis tempora, libero in fugiat, quisquam molestiae.</p>
+									<ul class="team-card-social">
+										<li class="social-item"><a href="#" target="_blank"><i class="fa fa-facebook"></i></a></li>
+										<li class="social-item"><a href="#" target="_blank"><i class="fa fa-twitter"></i></a></li>
+										<li class="social-item"><a href="#" target="_blank"><i class="fa fa-google-plus"></i></a></li>
+										<li class="social-item"><a href="#" target="_blank"><i class="fa fa-soundcloud"></i></a></li>
+										<li class="social-item"><a href="#" target="_blank"><i class="fa fa-linkedin"></i></a></li>
+										<li class="social-item"><a href="#" target="_blank"><i class="fa fa-link"></i></a></li>
+									</ul>
+								</div>
+							</div>
+						</div>
+
+						<!-- ===== TEAM CARD ===== -->
+						<div class="col-lg-4 col-lg-offset-0 col-md-8 col-md-offset-2 col-sm-12 mb-40">
+							<div class="team-card">
+								<figure class="team-card-image"><img src="assets/img/team/team-2.jpg" alt="Jana Doe" title="Jana Doe" /></figure>
+								<div class="team-card-content">
+									<h2 class="team-card-title">Jana Doe</h2>
+									<p class="team-card-job">Main Caster / Designer</p>
+									<p class="team-card-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat itaque optio minus perferendis tempora.</p>
+									<ul class="team-card-social">
+										<li class="social-item"><a href="#" target="_blank"><i class="fa fa-facebook"></i></a></li>
+										<li class="social-item"><a href="#" target="_blank"><i class="fa fa-twitter"></i></a></li>
+										<li class="social-item"><a href="#" target="_blank"><i class="fa fa-google-plus"></i></a></li>
+										<li class="social-item"><a href="#" target="_blank"><i class="fa fa-link"></i></a></li>
+									</ul>
+								</div>
+							</div>
+						</div>
+
+						<!-- ===== TEAM CARD ===== -->
+						<div class="col-lg-4 col-lg-offset-0 col-md-8 col-md-offset-2 col-sm-12 mb-40">
+							<div class="team-card">
+								<figure class="team-card-image"><img src="assets/img/team/team-3.jpg" alt="Joe Blow" title="Joe Blow" /></figure>
+								<div class="team-card-content">
+									<h2 class="team-card-title">Joe Blow</h2>
+									<p class="team-card-job">Supporter / Actor</p>
+									<p class="team-card-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat itaque optio minus perferendis tempora, libero in fugiat, quisquam molestiae.</p>
+									<ul class="team-card-social">
+										<li class="social-item"><a href="#" target="_blank"><i class="fa fa-facebook"></i></a></li>
+										<li class="social-item"><a href="#" target="_blank"><i class="fa fa-twitter"></i></a></li>
+										<li class="social-item"><a href="#" target="_blank"><i class="fa fa-google-plus"></i></a></li>
+										<li class="social-item"><a href="#" target="_blank"><i class="fa fa-linkedin"></i></a></li>
+										<li class="social-item"><a href="#" target="_blank"><i class="fa fa-link"></i></a></li>
+									</ul>
+								</div>
+							</div>
+						</div>
+
+					</div>
+
+				</div>
+			</section>
+
+			<!-- ===== NEWSLETTER ===== -->
+			<section class="section-positive text-center">
+				<div class="container">
+
+					<!-- ===== SECTION TITLE ===== -->
+					<h2 class="title-default">Never miss an episode</h2>
+					<p class="section-description">Form with MailChimp ready, just change the action! Lorem ipsum dolor sit amet, consectetur adipisicing elit. <br />  Vel modi rem laudantium, alias minus temporibus nihil reiciendis vero.</p>
+
+					<!-- ===== FORM NEWSLETTER ===== -->
+					<form action="//myaccount.us10.list-manage.com/subscribe/post?u=599a4d2b746f06b367c5f8085&amp;id=f429f2b2f5" method="POST" target="_blank" class="form-newsletter validate">
+						<fieldset class="row">
+							<div class="col-md-9 nopadding">
+								<input type="email" name="EMAIL" class="form-control form-negative" id="email-newsletter" placeholder="Your email" required />
+							</div>
+							<div class="col-md-3 nopadding">
+								<button class="btn btn-success btn-lg btn-block" type="submit"><i class="fa fa-paper-plane"></i></button>
+							</div>
+						</fieldset>
+					</form>
+
+				</div>
+			</section>
+
+			<!-- ===== DONATE ===== -->
+			<section id="donate" class="section-highlight text-center">
+				<div class="container">
+
+					<!-- ===== SECTION TITLE ===== -->
+					<h2 class="title-default">We need your help</h2>
+					<p class="section-description">Help us support the show! Lorem ipsum dolor sit amet, consectetur adipisicing elit. <br />  Vel modi rem laudantium, alias minus temporibus nihil reiciendis vero.</p>
+
+					<!-- ===== DONATE VALUES ===== -->
+					<div class="row mb-50">
+
+						<div class="col-lg-3 col-sm-6 mb-20">
+							<a href="#" class="btn btn-default btn-round btn-lg btn-block">U$ 5</a>
+						</div>
+
+						<div class="col-lg-3 col-sm-6 mb-20">
+							<a href="#" class="btn btn-default btn-round btn-lg btn-block">U$ 10</a>
+						</div>
+
+						<div class="col-lg-3 col-sm-6 mb-20">
+							<a href="#" class="btn btn-default btn-round btn-lg btn-block">U$ 25</a>
+						</div>
+
+						<div class="col-lg-3 col-sm-6 mb-20">
+							<a href="#" class="btn btn-default btn-round btn-lg btn-block">U$ 50</a>
+						</div>
+
+					</div>
+
+				</div>
+			</section>
   </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,239 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- ===== PODCAST LIST ===== -->
+			<section id="#episodes" class="section-positive">
+				<div class="container">
+
+					<!-- ===== SECTION TITLE ===== -->
+					<h2 class="title-default">Another Episodes</h2>
+
+					<div class="row">
+
+						<!-- ===== PODCAST CARD FULL ===== -->
+						<div class="col-sm-12 mb-40">
+							<div class="podcast-card full">
+								<figure class="podcast-image"><a href="podcast-soundcloud.html"><img src="assets/img/cards/podcast-full-thumb.jpg" alt="Seattle" title="Seattle" /></a></figure>
+								<div class="podcast-content">
+									<span class="podcast-date">December 21, 2015</span>
+									<h2 class="podcast-title"><a href="podcast-soundcloud.html">Episode #07 - Podcast embed from SoundCloud</a></h2>
+									<p class="podcast-excerpt"><a href="podcast-soundcloud.html">This is a full card example, porro culpa minus ipsam a accusantium, cupiditate expedita accusamus, perspiciatis magni aliquid cumque facilis rerum eius. Ipsum facilis iste repudiandae ducimus accusamus...</a></p>
+									<ul class="podcast-meta">
+										<li class="item"><a href="#" class="podcast-tag" rel="tag">Lifestyle</a></li>
+										<li class="item"><i class="fa fa-clock-o"></i> 37 mins.</li>
+										<li class="item"><a href="podcast-soundcloud.html" class="podcast-play"><i class="fa fa-play"></i> Play Episode</a></li>
+									</ul>
+								</div>
+							</div>
+						</div>
+
+						<!-- ===== PODCAST CARD BOXED ===== -->
+						<div class="col-sm-6 mb-40">
+							<div class="podcast-card boxed">
+								<figure class="podcast-image"><a href="podcast-video.html"><img src="assets/img/cards/podcast-boxed-thumb.jpg" alt="Biking" title="Biking" /></a></figure>
+								<div class="podcast-content">
+									<span class="podcast-date">December 19, 2015</span>
+									<h2 class="podcast-title"><a href="podcast-video.html">Episode #06 - You can also post a video</a></h2>
+									<p class="podcast-excerpt"><a href="podcast-video.html">This is a boxed podcast layout. Lorem ipsum dolor sit amet, consectetur adipisicing elit...</a></p>
+									<ul class="podcast-meta">
+										<li class="item"><a href="#" class="podcast-tag" rel="tag">Lifehacks</a></li>
+										<li class="item"><i class="fa fa-clock-o"></i> 51 mins.</li>
+										<li class="item"><a href="podcast-video.html" class="podcast-play"><i class="fa fa-play"></i> Play Episode</a></li>
+									</ul>
+								</div>
+							</div>
+						</div>
+
+						<!-- ===== PODCAST CARD BOXED ===== -->
+						<div class="col-sm-6 mb-40">
+							<div class="podcast-card boxed">
+								<figure class="podcast-image"><a href="podcast.html"><img src="assets/img/cards/podcast-boxed-thumb2.jpg" alt="Photography" title="Photography" /></a></figure>
+								<div class="podcast-content">
+									<span class="podcast-date">December 12, 2015</span>
+									<h2 class="podcast-title"><a href="podcast.html">Episode #05 - What I learned from photography</a></h2>
+									<p class="podcast-excerpt"><a href="podcast.html">This is a boxed podcast layout. Lorem ipsum dolor sit amet, consectetur adipisicing elit...</a></p>
+									<ul class="podcast-meta">
+										<li class="item"><a href="#" class="podcast-tag" rel="tag">Lifehacks</a></li>
+										<li class="item"><i class="fa fa-clock-o"></i> 29 mins.</li>
+										<li class="item"><a href="podcast.html" class="podcast-play"><i class="fa fa-play"></i> Play Episode</a></li>
+									</ul>
+								</div>
+							</div>
+						</div>
+
+						<!-- ===== PODCAST CARD BOXED ===== -->
+						<div class="col-sm-6 mb-40">
+							<div class="podcast-card boxed">
+								<figure class="podcast-image"><a href="podcast.html"><img src="assets/img/cards/podcast-boxed-thumb3.jpg" alt="Soccer" title="Soccer" /></a></figure>
+								<div class="podcast-content">
+									<span class="podcast-date">December 9, 2015</span>
+									<h2 class="podcast-title"><a href="podcast.html">Episode #04 - How we decide to play soccer</a></h2>
+									<p class="podcast-excerpt"><a href="podcast.html">This is a boxed podcast layout. Lorem ipsum dolor sit amet, consectetur adipisicing elit...</a></p>
+									<ul class="podcast-meta">
+										<li class="item"><a href="#" class="podcast-tag" rel="tag">Sports</a></li>
+										<li class="item"><i class="fa fa-clock-o"></i> 40 mins.</li>
+										<li class="item"><a href="podcast.html" class="podcast-play"><i class="fa fa-play"></i> Play Episode</a></li>
+									</ul>
+								</div>
+							</div>
+						</div>
+
+						<!-- ===== PODCAST CARD BOXED ===== -->
+						<div class="col-sm-6 mb-40">
+							<div class="podcast-card boxed">
+								<figure class="podcast-image"><a href="podcast.html"><img src="assets/img/cards/podcast-boxed-thumb4.jpg" alt="Music Show" title="Music Show" /></a></figure>
+								<div class="podcast-content">
+									<span class="podcast-date">November 27, 2015</span>
+									<h2 class="podcast-title"><a href="podcast.html">Episode #03 - The best show of our lifes</a></h2>
+									<p class="podcast-excerpt"><a href="podcast.html">This is a boxed podcast layout. Lorem ipsum dolor sit amet, consectetur adipisicing elit...</a></p>
+									<ul class="podcast-meta">
+										<li class="item"><a href="#" class="podcast-tag" rel="tag">Lifestyle</a></li>
+										<li class="item"><i class="fa fa-clock-o"></i> 18 mins.</li>
+										<li class="item"><a href="podcast.html" class="podcast-play"><i class="fa fa-play"></i> Play Episode</a></li>
+									</ul>
+								</div>
+							</div>
+						</div>
+
+						<!-- ===== CHECK MORE ===== -->
+						<div class="col-sm-12 mb-50">
+							<a href="podcasts.html" class="btn btn-primary btn-block btn-lg">View more episodes</a>
+						</div>
+
+					</div>
+
+				</div>
+			</section>
+
+			<!-- ===== ABOUT US ===== -->
+			<section id="about-us" class="section-negative">
+				<div class="container">
+
+					<!-- ===== SECTION TITLE ===== -->
+					<h2 class="title-default text-center">Behind the mic...</h2>
+
+					<div class="row mb-30">
+
+						<!-- ===== TEAM CARD ===== -->
+						<div class="col-lg-4 col-lg-offset-0 col-md-8 col-md-offset-2 col-sm-12 mb-40">
+							<div class="team-card">
+								<figure class="team-card-image"><img src="assets/img/team/team-1.jpg" alt="John Doe" title="John Doe" /></figure>
+								<div class="team-card-content">
+									<h2 class="team-card-title">John Doe</h2>
+									<p class="team-card-job">Main Caster / Web Developer</p>
+									<p class="team-card-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat itaque optio minus perferendis tempora, libero in fugiat, quisquam molestiae.</p>
+									<ul class="team-card-social">
+										<li class="social-item"><a href="#" target="_blank"><i class="fa fa-facebook"></i></a></li>
+										<li class="social-item"><a href="#" target="_blank"><i class="fa fa-twitter"></i></a></li>
+										<li class="social-item"><a href="#" target="_blank"><i class="fa fa-google-plus"></i></a></li>
+										<li class="social-item"><a href="#" target="_blank"><i class="fa fa-soundcloud"></i></a></li>
+										<li class="social-item"><a href="#" target="_blank"><i class="fa fa-linkedin"></i></a></li>
+										<li class="social-item"><a href="#" target="_blank"><i class="fa fa-link"></i></a></li>
+									</ul>
+								</div>
+							</div>
+						</div>
+
+						<!-- ===== TEAM CARD ===== -->
+						<div class="col-lg-4 col-lg-offset-0 col-md-8 col-md-offset-2 col-sm-12 mb-40">
+							<div class="team-card">
+								<figure class="team-card-image"><img src="assets/img/team/team-2.jpg" alt="Jana Doe" title="Jana Doe" /></figure>
+								<div class="team-card-content">
+									<h2 class="team-card-title">Jana Doe</h2>
+									<p class="team-card-job">Main Caster / Designer</p>
+									<p class="team-card-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat itaque optio minus perferendis tempora.</p>
+									<ul class="team-card-social">
+										<li class="social-item"><a href="#" target="_blank"><i class="fa fa-facebook"></i></a></li>
+										<li class="social-item"><a href="#" target="_blank"><i class="fa fa-twitter"></i></a></li>
+										<li class="social-item"><a href="#" target="_blank"><i class="fa fa-google-plus"></i></a></li>
+										<li class="social-item"><a href="#" target="_blank"><i class="fa fa-link"></i></a></li>
+									</ul>
+								</div>
+							</div>
+						</div>
+
+						<!-- ===== TEAM CARD ===== -->
+						<div class="col-lg-4 col-lg-offset-0 col-md-8 col-md-offset-2 col-sm-12 mb-40">
+							<div class="team-card">
+								<figure class="team-card-image"><img src="assets/img/team/team-3.jpg" alt="Joe Blow" title="Joe Blow" /></figure>
+								<div class="team-card-content">
+									<h2 class="team-card-title">Joe Blow</h2>
+									<p class="team-card-job">Supporter / Actor</p>
+									<p class="team-card-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat itaque optio minus perferendis tempora, libero in fugiat, quisquam molestiae.</p>
+									<ul class="team-card-social">
+										<li class="social-item"><a href="#" target="_blank"><i class="fa fa-facebook"></i></a></li>
+										<li class="social-item"><a href="#" target="_blank"><i class="fa fa-twitter"></i></a></li>
+										<li class="social-item"><a href="#" target="_blank"><i class="fa fa-google-plus"></i></a></li>
+										<li class="social-item"><a href="#" target="_blank"><i class="fa fa-linkedin"></i></a></li>
+										<li class="social-item"><a href="#" target="_blank"><i class="fa fa-link"></i></a></li>
+									</ul>
+								</div>
+							</div>
+						</div>
+
+					</div>
+
+				</div>
+			</section>
+
+			<!-- ===== NEWSLETTER ===== -->
+			<section class="section-positive text-center">
+				<div class="container">
+
+					<!-- ===== SECTION TITLE ===== -->
+					<h2 class="title-default">Never miss an episode</h2>
+					<p class="section-description">Form with MailChimp ready, just change the action! Lorem ipsum dolor sit amet, consectetur adipisicing elit. <br />  Vel modi rem laudantium, alias minus temporibus nihil reiciendis vero.</p>
+
+					<!-- ===== FORM NEWSLETTER ===== -->
+					<form action="//myaccount.us10.list-manage.com/subscribe/post?u=599a4d2b746f06b367c5f8085&amp;id=f429f2b2f5" method="POST" target="_blank" class="form-newsletter validate">
+						<fieldset class="row">
+							<div class="col-md-9 nopadding">
+								<input type="email" name="EMAIL" class="form-control form-negative" id="email-newsletter" placeholder="Your email" required />
+							</div>
+							<div class="col-md-3 nopadding">
+								<button class="btn btn-success btn-lg btn-block" type="submit"><i class="fa fa-paper-plane"></i></button>
+							</div>
+						</fieldset>
+					</form>
+
+				</div>
+			</section>
+
+			<!-- ===== DONATE ===== -->
+			<section id="donate" class="section-highlight text-center">
+				<div class="container">
+
+					<!-- ===== SECTION TITLE ===== -->
+					<h2 class="title-default">We need your help</h2>
+					<p class="section-description">Help us support the show! Lorem ipsum dolor sit amet, consectetur adipisicing elit. <br />  Vel modi rem laudantium, alias minus temporibus nihil reiciendis vero.</p>
+
+					<!-- ===== DONATE VALUES ===== -->
+					<div class="row mb-50">
+
+						<div class="col-lg-3 col-sm-6 mb-20">
+							<a href="#" class="btn btn-default btn-round btn-lg btn-block">U$ 5</a>
+						</div>
+
+						<div class="col-lg-3 col-sm-6 mb-20">
+							<a href="#" class="btn btn-default btn-round btn-lg btn-block">U$ 10</a>
+						</div>
+
+						<div class="col-lg-3 col-sm-6 mb-20">
+							<a href="#" class="btn btn-default btn-round btn-lg btn-block">U$ 25</a>
+						</div>
+
+						<div class="col-lg-3 col-sm-6 mb-20">
+							<a href="#" class="btn btn-default btn-round btn-lg btn-block">U$ 50</a>
+						</div>
+
+					</div>
+
+				</div>
+			</section>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
+  name: 'home'
 }
 </script>
