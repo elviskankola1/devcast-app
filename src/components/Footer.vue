07a7e5b0481415c7a6b9923ad79175dd9542502b
bernard-ng <ngandubernard@gmail.com>
2018-12-25 13:14:06 +0200

    [Update] add the template code in the footer component

---
+++ src/components/Footer.vue
@@ -0,0 +1,95 @@
+<template>
+		<footer class="footer">
+
+			<!-- ===== FOOTER CONTENT INFORMATION ===== -->
+			<section class="section-positive">
+				<div class="container">
+
+					<div class="row mt-70 mb-30">
+
+						<!-- ===== SITEMAP ===== -->
+						<div class="col-sm-3 mb-40">
+							<h2 class="title-separator white">Sitemap</h2>
+							<ul class="footer-list">
+								<li><a href="index.html">Home</a></li>
+								<li><a href="podcasts.html">Podcasts</a></li>
+								<li><a href="about-us.html">About Us</a></li>
+								<li><a href="contact.html">Contact</a></li>
+							</ul>
+						</div>
+
+						<!-- ===== PODCASTS EPISODES ===== -->
+						<div class="col-sm-3 mb-40">
+							<h2 class="title-separator white">Lastest Episodes</h2>
+							<ul class="footer-complement">
+								<li>
+									<a href="podcast.html">#08 - Our vacations have been so amazing!</a>
+									<span>December 22, 2015</span>
+								</li>
+								<li>
+									<a href="podcast-soundcloud.html">#07 - Podcast embed from SoundCloud</a>
+									<span>December 21, 2015</span>
+								</li>
+							</ul>
+						</div>
+
+						<!-- ===== PODCASTS EPISODES ===== -->
+						<div class="col-sm-3 mb-40">
+							<h2 class="title-separator white">Who is helping?</h2>
+							<ul class="footer-complement">
+								<li>
+									<a href="#">@reidarking</a>
+									<span>Donation - U$ 10</span>
+								</li>
+								<li>
+									<a href="#">@reidarking</a>
+									<span>Donation - U$ 5</span>
+								</li>
+								<li>
+									<a href="#">@reidarking</a>
+									<span>Donation - U$ 50</span>
+								</li>
+							</ul>
+						</div>
+
+						<!-- ===== SOCIAL CONNECTION ===== -->
+						<div class="col-sm-3 mb-40">
+							<h2 class="title-separator white">We are social</h2>
+							<ul class="social-list">
+								<li class="social-item"><a href="#" target="_blank"><i class="fa fa-facebook"></i></a></li>
+								<li class="social-item"><a href="#" target="_blank"><i class="fa fa-twitter"></i></a></li>
+								<li class="social-item"><a href="#" target="_blank"><i class="fa fa-instagram"></i></a></li>
+								<li class="social-item"><a href="#" target="_blank"><i class="fa fa-google-plus"></i></a></li>
+								<li class="social-item"><a href="#" target="_blank"><i class="fa fa-soundcloud"></i></a></li>
+								<li class="social-item"><a href="#" target="_blank"><i class="fa fa-pinterest"></i></a></li>
+								<li class="social-item"><a href="#" target="_blank"><i class="fa fa-youtube"></i></a></li>
+								<li class="social-item"><a href="#" target="_blank"><i class="fa fa-spotify"></i></a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</section>
+
+			<!-- ===== FOOTER INFORMATION ===== -->
+			<section class="footer-credits">
+				<div class="container">
+
+					<div class="row">
+						<div class="col-sm-6 footer-logo">
+							<h2><a href="index.html"><img src="assets/logo.png" alt="Soudcast - Podcast Responsive Theme" title="Soundcast - Podcast Responsive Theme" /></a></h2>
+						</div>
+
+						<div class="col-sm-6 text-right">
+							<a href="https://github.com/bernard-ng">bernard-ng</a> - 2018. All rights reserved.
+						</div>
+					</div>
+				</div>
+			</section>
+		</footer>
+</template>
+
+<script>
+export default {
+  name: 'Footer'
+}
+</script>
