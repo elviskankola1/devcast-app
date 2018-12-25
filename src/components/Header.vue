a3fddce5640d8c9fb777bf4964940f42c2c71de0
bernard-ng <ngandubernard@gmail.com>
2018-12-25 13:14:43 +0200

    [Update] add the template code in the header component

---
+++ src/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+  <header class="header absolute" itemscope itemtype="http://schema.org/Organization">
+			<div class="container">
+				<h1 class="logo"><a href="index.html"><img itemprop="logo" src="assets/logo.png" alt="logo" /></a></h1>
+
+				<nav class="navigation">
+					<ul class="menu">
+						<li class="menuitem active"><router-link to="/">Home</router-link></li>
+						<li class="menuitem"><router-link to="/podcasts">Podcast</router-link></li>
+						<li class="menuitem"><router-link to="/about">About</router-link></li>
+						<li class="menuitem"><router-link to="/contact">contact</router-link></li>
+					</ul>
+
+					<a href="#" class="btn-hamburguer-menu"><i class="fa fa-bars"></i></a>
+				</nav>
+
+			</div>
+		</header>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
