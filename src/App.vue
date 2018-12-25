f8e246b2e4381a085d0eb601995c6637059702ef
bernard-ng <ngandubernard@gmail.com>
2018-12-25 13:15:16 +0200

    [Update] structure the Html code

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,46 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <Header></Header>
+
+    <section class="podcast-hero" style="background-image: url(http://127.0.0.1/photofills)">
+			<div class="podcast-hero-inner">
+				<div class="container">
+						<div class="podcast-hero-content">
+						<span class="podcast-hero-date">December 22, 2015</span>
+						<h2 class="podcast-hero-title"><a href="podcast.html">Episode #08 - Our vacations have been so amazing!</a></h2>
+						<ul class="podcast-hero-meta">
+							<li class="item"><a href="#" class="podcast-hero-tag" rel="tag">Lifestyle</a></li>
+							<li class="item"><i class="fa fa-clock-o"></i> 45 mins.</li>
+							<li class="item"><a href="#" class="podcast-hero-download"><i class="fa fa-download"></i> Download</a></li>
+						</ul>
+					</div>
+				</div>
+
+				<div class="podcast-hero-player-content">
+					<div class="container">
+						<audio src="assets/audio/emotional.mp3"></audio>
+					</div>
+				</div>
+
+			</div>
+		</section>
+
+    <main id="main" class="main">
+      <router-view/>
+    </main>
+    <Footer></Footer>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+
+<script>
+import Header from '@/components/Header.vue'
+import Footer from '@/components/Footer.vue'
+
+export default {
+  components: {
+    Header,
+    Footer
   }
 }
-</style>
+</script>
