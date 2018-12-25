7907fe956da7d8bb712f745f3a718308007a7c92
bernard-ng <ngandubernard@gmail.com>
2018-12-25 14:05:49 +0200

    [Update] add the template code in 404.vue

---
+++ src/views/404.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="404">
+    <section class="page-error" style="background-image: url(assets/img/error-background.jpg)">
+				<div class="page-error-inner">
+					<div class="container">
+						<div class="page-error-content text-center">
+							<h2>Page not Found</h2>
+							<p class="mb-40">The page you are looking for doesn't exist. <br /> Don't worry... It happens to the best of us.</p>
+              <router-link class="btn btn-info" to="/">Back to home</router-link>
+						</div>
+					</div>
+				</div>
+			</section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: '404'
+}
+</script>
+
