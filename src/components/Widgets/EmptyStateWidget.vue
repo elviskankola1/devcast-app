21eba99c0b6286f1ac3e4ed4fa7e995994a0cc4e
bernard-ng <ngandubernard@gmail.com>
2018-12-26 14:42:06 +0200

    [New] create widgets for showing some informations

---
+++ src/components/Widgets/EmptyStateWidget.vue
@@ -0,0 +1,19 @@
+<template>
+<section class="page-error" style="background-image: url(assets/img/error-background.jpg)">
+  <div class="page-error-inner">
+    <div class="container">
+      <div class="page-error-content text-center">
+        <h2>Nothing to show</h2>
+        <p class="mb-40">The page you are looking for doesn't have data to show. <br /> Don't worry... It happens to the best of us.</p>
+        <router-link class="btn btn-info" :to="{name: 'home'}">Back to home</router-link>
+      </div>
+    </div>
+  </div>
+</section>
+</template>
+
+<script>
+export default {
+  name: 'EmptyStateWidget'
+}
+</script>
---
+++ src/components/Widgets/ErrorWidgets.vue
@@ -0,0 +1,23 @@
+<template>
+<section class="page-error" style="background-image: url(assets/img/error-background.jpg)">
+  <div class="page-error-inner">
+    <div class="container">
+      <div class="page-error-content text-center">
+        <h2>{{ error }}</h2>
+        <p class="mb-40">{{ message }}</p>
+        <router-link class="btn btn-info" :to="{name: 'home'}">Back to home</router-link>
+      </div>
+    </div>
+  </div>
+</section>
+</template>
+
+<script>
+export default {
+  name: 'ErrorWidget',
+  props: {
+    error: {String, default: 'Oups Something went wrong'},
+    message: {String, default: ''}
+  }
+}
+</script>
