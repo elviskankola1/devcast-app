3e50643871e7db8d55233eb36f6ac6cf462e6cb5
bernard-ng <ngandubernard@gmail.com>
2018-12-26 03:02:19 +0200

    [Update] use of the truncate filter

--- src/components/Podcasts/PodcastCard.vue
+++ src/components/Podcasts/PodcastCard.vue
@@ -2,19 +2,19 @@
   <div class="podcast-card" :class="type">
     <figure class="podcast-image">
       <router-link :to="{name: 'podcasts.show', params:{id: p.id, slug: p.slug}}">
-        <img :src="p.thumb.small" :alt="p.name" :title="p.name" />
+        <img :src="p.thumb" :alt="p.name" :title="p.name" />
       </router-link>
     </figure>
     <div class="podcast-content">
       <span class="podcast-date">{{ p.created_at }}</span>
       <h2 class="podcast-title">
         <router-link :to="{name: 'podcasts.show', params:{id: p.id, slug: p.slug}}">
-          {{ p.name }}
+          {{ p.name | truncate(40) }}
         </router-link>
       </h2>
       <p class="podcast-excerpt">
         <router-link :to="{name: 'podcasts.show', params:{id: p.id, slug: p.slug}}">
-          {{ p.description }}
+          {{ p.description | truncate(100) }}
         </router-link>
       </p>
       <ul class="podcast-meta">
@@ -23,7 +23,7 @@
             {{ p.category }}
           </router-link>
         </li>
-        <li class="item"><i class="fa fa-clock-o"></i>{{ p.duration }}</li>
+        <li class="item"><i class="fa fa-clock-o"></i> {{ p.duration }} m</li>
         <li class="item">
           <router-link :to="{name: 'podcasts.show', params:{id: p.id, slug: p.slug}}" class="podcast-play">
             <i class="fa fa-play"></i> Play Episode
