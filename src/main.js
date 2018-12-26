aaf7f6897be4d1aaaa096292b6dc5370bd28d95f
bernard-ng <ngandubernard@gmail.com>
2018-12-26 03:04:33 +0200

    [Update] use the new vue-truncate-filter

--- src/main.js
+++ src/main.js
@@ -2,9 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import VueResource from 'vue-resource'
+import VueTruncateFilter from 'vue-truncate-filter'
 import './registerServiceWorker'
 
 Vue.config.productionTip = false
+Vue.use(VueResource)
+Vue.use(VueTruncateFilter)
 
 new Vue({
   router,
