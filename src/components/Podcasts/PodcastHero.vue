b81a92aa3e11d8641cc3460ddabf3c5005bc2982
bernard-ng <ngandubernard@gmail.com>
2018-12-26 12:03:26 +0200

    [Update] add the truncate-filter

--- src/components/Podcasts/PodcastHero.vue
+++ src/components/Podcasts/PodcastHero.vue
@@ -6,17 +6,17 @@
       <span class="podcast-hero-date">{{ p.created_at }}</span>
       <h2 class="podcast-hero-title">
         <router-link :to="{name: 'podcasts.show', params:{id: p.id, slug: p.slug}}">
-          {{ p.name }}
+          {{ p.name | truncate(30) }}
         </router-link>
       </h2>
-      <p>{{ p.description }}</p>
+      <p>{{ p.description | truncate(150) }}</p>
       <ul class="podcast-hero-meta">
         <li class="item">
           <router-link :to="{name: 'categories.show', params:{name: p.category}}" class="podcast-hero-tag" rel="tag">
             {{ p.category }}
           </router-link>
         </li>
-        <li class="item"><i class="fa fa-clock-o"></i>{{ p.duration }}</li>
+        <li class="item"><i class="fa fa-clock-o"></i> {{ p.duration }}</li>
         <li class="item">
           <a :href="p.audio" class="podcast-hero-download">
             <i class="fa fa-download"></i> Download
